9076be282762b48dc47025c8eaadc5ea704fc8de
astyer22 <astyer@byui.edu>
2024-03-03 22:00:48 -0700

    Letter grade push

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.WriteLine("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter = "c";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter};");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine($"Your passed the class with a {letter}");
+        }
+
+        else 
+        {
+            Console.WriteLine("Work harder nbext semester");
+        }
+    }  
 }
